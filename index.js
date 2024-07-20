5f84f31d0792938e3cad1142a6473264d559d613
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-20 14:49:39 -0300

    feat: DB integrado com o endpoint `Update`

--- index.js
+++ index.js
@@ -54,27 +54,29 @@ async function main() {
     res.status(201).send(novoCard)
   })
 
-  app.put('/card/:id', function (req, res) {
+  app.put('/card/:id', async function (req, res) {
     const id = req.params.id
 
-    if (!card[id - 1]) {
-      return res.status(404).send('Item não encontrado.')
-    }
+    // if (!card[id - 1]) {
+    //   return res.status(404).send('Item não encontrado.')
+    // }
 
-    const body = req.body
-    const novoCard = body.title
+    const novoCard = req.body
 
-    if (!novoCard) {
+    if (!novoCard || !novoCard.title) {
       return res.status(400).send('A requisição deve conter a propriedade `title`.')
     }
 
-    if (card.includes(novoCard)) {
-      return res.status(409).send('Título já existente para um Card.')
-    }
+    // if (card.includes(novoCard)) {
+    //   return res.status(409).send('Título já existente para um Card.')
+    // }
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: novoCard }
+    )
+    // card[id - 1] = novoCard
 
-    card[id - 1] = novoCard
-
-    res.send('Card atualizado com sucesso: ' + id + ' - ' + novoCard)
+    res.send(novoCard)
   })
 
   app.delete('/card/:id', function (req, res) {
