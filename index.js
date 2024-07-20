6c9fe23da301c2e1369ec13335a1182b2088786a
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-20 14:52:39 -0300

    feat: DB integrado com o endpoint `Delete`

--- index.js
+++ index.js
@@ -79,14 +79,15 @@ async function main() {
     res.send(novoCard)
   })
 
-  app.delete('/card/:id', function (req, res) {
+  app.delete('/card/:id', async function (req, res) {
     const id = req.params.id
 
-    if (!card[id - 1]) {
-      return res.status(404).send('Item não encontrado.')
-    }
+    // if (!card[id - 1]) {
+    //   return res.status(404).send('Item não encontrado.')
+    // }
 
-    delete card[id - 1]
+    await collection.deleteOne({ _id: new ObjectId(id) })
+    // delete card[id - 1]
 
     res.send('Card removido com sucesso ' + id)
   })
