8837b4467cc2caa4d29a8db6347a5829a0d7e58f
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-20 02:59:31 -0300

    feat: endpoint `Read By ID GET /card/:id` implementado

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (_, res) {
-    res.send('Hello, World!')
+  res.send('Hello, World!')
 })
 
 const card = ['Menu Online', 'Sorteios X', 'Educação Infantil', 'Calc IMC']
 
 app.get('/card', function (_, res) {
-    res.send(card)
+  res.send(card)
+})
+
+app.get('/card/:id', function (req, res) {
+  const id = req.params.id
+  const item = card[id - 1]
+
+  res.send(item)
 })
 
 app.listen(3001, function () {
-    console.log('Aplicação rodando na porta https://localhost:3001')
+  console.log('Aplicação rodando na porta https://localhost:3001')
 })
