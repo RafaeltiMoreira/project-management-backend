39405f15f56f7a58e0c7a0b0f3f96e5bb7845c2c
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-20 22:57:34 -0300

    refactor: atualização do `DB`

--- src/card/card.entity.js
+++ src/card/card.entity.js
@@ -6,6 +6,19 @@ const card = Joi.object({
     .min(3)
     .max(30)
     .required(),
+
+  description: Joi.string()
+    .min(3)
+    .max(250)
+    .required(),
+
+  image: Joi.string()
+    .uri()
+    .required(),
+
+  deploy: Joi.string()
+    .uri()
+    .required(),
 });
 
 module.exports = card
