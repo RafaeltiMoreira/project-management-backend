327f411c30ec111b106f26ad446fbcab5a85e920
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-20 21:36:56 -0300

    feat: implementação sobre `Erros` e `Erros assíncronos`

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { connectToDatabase } = require('./db/database-connection');
 const cardRouter = require('./card/card.router');
 const cors = require('cors');
+require('express-async-errors');
 
 async function main() {
   connectToDatabase()
@@ -16,6 +17,11 @@ async function main() {
   })
 
   app.use('/card', cardRouter)
+
+  app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).send({ error: 'Negado, algo deu errado!' });
+  })
 
   app.listen(3001, function () {
     console.log('Aplicação rodando na porta https://localhost:3001')
